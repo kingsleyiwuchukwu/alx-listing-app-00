2b7c046967ef4a44750833132754ec9a09b2ca3a
kingsleyiwuchukwu <kingsleyiwuchukwu0@gmail.com>
2025-07-19 23:31:20 +0100

    Build Responsive Listing Page with Tailwind CSS

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -8,3 +8,21 @@ export interface ButtonProps {
   text: string;
   onClick?: () => void;
 }
+
+export interface PropertyProps {
+  name: string;
+  address: {
+    state: string;
+    city: string;
+    country: string};
+  rating: number;
+  category: string[];
+  price: number;
+  offers: {
+    bed: string;
+    shower: string;
+    occupants: string;};
+  image: string;
+  discount?: string;
+
+}
